a42139b334675726e2426d4769cb5d5afcf9dacd
snehalbhagat2005 <snehalbhagat0000@gmail.com>
2026-08-21 22:54:39 +0530

    Time: 0 ms (100%), Space: 41.9 MB (85.89%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int fib(int n) {
+        int a=0;
+        int b=1;
+
+        if( n<=1){
+            return n;
+        }
+        int c=0;
+        for(int i=1;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+}
