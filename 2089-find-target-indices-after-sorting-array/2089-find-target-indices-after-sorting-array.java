bb79236616cfdeb6092dbbd1d9113a6b531552ba
snehalbhagat2005 <snehalbhagat0000@gmail.com>
2026-08-24 20:11:49 +0530

    Time: 6 ms (28.46%), Space: 45 MB (70.41%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -4,10 +4,52 @@ class Solution {
         Arrays.sort(arr);
         List<Integer> ans=new ArrayList<>();
 
-        for(int i=0;i<n;i++){
-            if(arr[i]==target){
-                ans.add(i);
+        // for(int i=0;i<n;i++){
+        //     if(arr[i]==target){
+        //         ans.add(i);
+        //     }
+        // }
+
+        int start =0;
+        int end=n-1;
+        int index=-1;
+
+        while(start<=end){
+            int mid=(int)(start+end)/2;
+            if(arr[mid]==target){
+                index=mid;
+                end=mid-1;
             }
+            else if(arr[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+
+        int lastIndex=-1;
+
+        start =0;
+        end=n-1;
+        while(start<=end){
+            int mid=(int)(start+end)/2;
+            if(arr[mid]==target){
+                lastIndex=mid;
+                start=mid+1;
+            }
+            else if(arr[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+
+        if(index==-1) return ans;
+
+        for(int i=index;i<=lastIndex;i++){
+            ans.add(i);
         }
 
         return ans;
